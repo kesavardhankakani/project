4e5fab51367a730421bee7f81579b35db5ce1700
kesavardhankakani <kesavardhankakani@email.com>
2026-07-03 19:01:28 +0530

    done

--- new.py
+++ new.py
@@ -31,8 +31,25 @@ def create_users_table():
     connection.commit()
     cur.close()
     connection.close()
-create_users_table()
 
+def create_note_table():
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("""
+        create table IF NOT EXISTS note(noteid SERIAL PRIMARY KEY,
+                userid int REFERENCES users_table(userid),
+                title varchar(100) NOT NULL,
+                description text NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+""")
+    connection.commit()
+    cur.close()
+    connection.close()
+     
+     
+create_users_table()
+create_note_table()
 SECRET_KEY = "this is my keyy"
 def create_jwt(userid,username):
      payload={
@@ -43,6 +60,13 @@ def create_jwt(userid,username):
      token = jwt.encode(payload,SECRET_KEY,algorithm ="HS256")
      return token
 
+def verify_jwt(token):
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return data
+    except:
+         return None
+    
 @app.route("/register",methods =['POST'])
 def register():
     username = request.json.get("username")
@@ -108,5 +132,37 @@ def login():
                   "collegename":user[5]
             }),200
       return jsonify({"message":"invalid password"}),200
+      
+@app.route("/create_note", methods=['POST'])
+def create_note():
+    token = request.headers.get("Authorization")
+    print(token)
+    if not token:
+        return jsonify({"error": "Token required"}), 401
+    user_data = verify_jwt(token)
+    if user_data is None:
+        return jsonify({"error": "Invalid or expired token"}), 401
+    userid = user_data["userid"]
+    username = user_data["username"]
+    title = request.json.get('title')
+    description = request.json.get('description')
+    if not title or not description:
+        return jsonify({"error": "All fields required"}), 400
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("""
+        insert into note(userid,title,description)
+        VALUES(%s,%s,%s);
+    """, (userid, title, description))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return jsonify({
+        "message": "Note creation successfull..",
+        "user_id": userid,
+        "username":username
+    }), 201
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
